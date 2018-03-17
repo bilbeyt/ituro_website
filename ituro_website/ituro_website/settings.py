b82d4b7cfaf0c89c7353a7a24a89e5a85ab7338e
bilbeyt <bilbeyt@gmail.com>
2018-03-17 13:47:48 +0300

    debug

--- ituro_website/ituro_website/settings.py
+++ ituro_website/ituro_website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vt341(sudt_qaz19l^_-15328np@yu)#u=#$+g(f@k%8pomf$%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ituro.org']
 
 
 # Application definition
