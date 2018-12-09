185693c33b3f5b7ba32e9d596ae1e8081be386a5
batuortal <batuhanortal@gmail.com>
2018-12-09 21:13:40 +0300

    Added contactView and successView

--- ituro_website/post/views.py
+++ ituro_website/post/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.mail import send_mail, BadHeaderError
 from django.utils.translation import get_language
 from django.utils.translation import activate
+from django.template.loader import get_template
 from post.models import CommonEntry
 from post.models import NewsEntry
 from post.models import CategoryEntry
 from post.models import HomePageEntry
 from post.models import AboutEntry
 from post.models import SponsorshipEntry
-
+from forms import ContactForm
 
 class HomePageDetailView(TemplateView):
     template_name = "homepage.html"
@@ -113,3 +115,23 @@ class SponsorshipDetailView(DetailView):
         else:
             context = self.get_context_data(object=self.object)
             return self.render_to_response(context)
+
+
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['batuortal@gmail.com', None])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success/')
+    return render(request, "contact.html", {'form': form})
+
+def successView(request):
+    return render(request, "success.html")
