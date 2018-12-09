559435818918b533cbdb228060ef014dd7fe5ef0
batuortal <batuhanortal@gmail.com>
2018-12-09 21:15:56 +0300

    Added Contact Form

---
+++ ituro_website/post/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class ContactForm(forms.Form):
+    from_email = forms.EmailField(widget=forms.TextInput(
+        attrs={
+            "class": "contact_textline",
+	    "placeholder": _("Enter your email here.")
+	}
+    ), label=_("Sender Email"), required=True)
+    subject = forms.CharField(widget=forms.TextInput(
+	attrs={
+	    "class": "contact_textline",
+	    "placeholder": _("Enter your name here.")
+	}
+    ), label=_("Sender Name"), required=True)
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={
+	    "class": "contact_textarea",
+	    "placeholder": _("Enter your message here.")
+	}
+    ), label=_("Message"), required=True)
