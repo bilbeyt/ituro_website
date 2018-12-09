e6f11ef8681cdfceecc83a2cfdeb1632280ebaa4
batuortal <batuhanortal@gmail.com>
2018-12-09 21:11:21 +0300

    Added Contact us and Success urls

--- ituro_website/post/urls.py
+++ ituro_website/post/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 from post.views import HomePageDetailView, AboutDetailView, \
-    CategoryDetailView, SponsorshipDetailView, CommonDetailView, NewsEntryDetailView
+    CategoryDetailView, SponsorshipDetailView, CommonDetailView, \
+    NewsEntryDetailView, contactView, successView
 
 
 urlpatterns = [
 
     url(r'^$', HomePageDetailView.as_view(), name="homepage"),
+    url(_(r'^contact_us/$'), contactView, name="contact"),
+    url(_(r'^success/$'), successView, name="success"),
     url(_(r'^about/(?P<slug>[-_\w]+)/$'), AboutDetailView.as_view(),
         name="about_detail"),
     url(_(r'^category/(?P<slug>[-_\w]+)/$'), CategoryDetailView.as_view(),
